3fc7bb333f97e0806633bbe982cb908d8a0ab1be
Amitav Roy <reachme@amitavroy.com>
2013-09-24 15:38:18 +0530

    User login functionality now working. Cookie is now getting created for the user with Session name and Session ID as we are getting from the user login data return.

--- js/module.js
+++ js/module.js
@@ -17,65 +17,40 @@ mi.config(['$routeProvider', function($routeProvider) {
 mi.factory('sharedUser', ['$http', '$cookieStore', '$rootScope', function($http, $cookieStore, $rootScope) {
   var user = {};
   user.token = "";
+  user.userObject = {};
 
+  /*doing the user login*/
   user.login = function(username, password) {
-    var token = this.getToken();
     return $http({
-      headers: {'X-CSRF-Token' : token, 'Content-Type': 'application/json'},
+      headers: {'Content-Type': 'application/json'},
       url: loginUrl,
       method: "POST",
       data: {username: username, password: password}
     }).success(function(data) {
-        var cookieData = {
-          sessionId: data.sessid,
-          session_name: data.session_name,
-          token: this.token
-        };
-        $cookieStore.put('auth',JSON.stringify(cookieData));
+        $cookieStore.put('auth', 1);
+        $cookieStore.put(data.session_name, data.sessid);
       });
   };
-
-  user.getToken = function() {
-    return this.token === "" ? this.setToken() : this.token;
-  }
-
-  user.setToken = function() {
-    this.token = $http({
-      headers: {'Content-Type': 'application/json'},
-      url: tokenUrl,
-      method: "POST",
-      data: ""
-    }).success(function(data) {
-        this.token = data;
-        console.log(this.token);
-        $rootScope.$broadcast('handleToken',this.token);
-      });
-  }
 
   return user;
 }]);
 
 mi.controller('loginCtrl', function($scope, sharedUser, $location, $rootScope) {
-  $scope.token = "";
-  $scope.doLogin = function(formData) {
-    var username = formData.username;
-    var password = formData.pass;
+  $scope.user = "";
+  $scope.doLogin = function(form) {
+    var username = form.username;
+    var password = form.pass;
 
     sharedUser.login(username, password).then(function(data) {
-      $scope.user = data;
-      $rootScope.$broadcast('handleUserBroadcast',$scope.user);
-    })
+      $location.path('/home');
+    });
   };
 
-  $scope.$on('handleToken', function(event, projects) {
-    $scope.token = projects;
-    $location.path('/home');
-  });
 });
 
-mi.controller('homeCtrl', function($scope, sharedUser) {
-
-  $scope.$on('handleUserBroadcast', function(event, user) {
-    $scope.user = user;
-  });
+mi.controller('homeCtrl', function($scope, sharedUser, $cookieStore, $location) {
+  var auth = $cookieStore.get('auth');
+  if (auth != 1) {
+    $location.path('/')
+  }
 });
